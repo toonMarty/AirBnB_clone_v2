52f0d46a904962e41ff81660e96325efb8b148a7
toonMarty <martinnyaga7373@gmail.com>
2022-11-11 07:20:24 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
 # a Bash script that sets up the web servers for the deployment of web_static
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
+
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+mkdir -p /data/web_static/shared/
+echo "Hello World" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	add_header X-Served-By $HOSTNAME;
+	root /var/www/html;
+	index index.html index.htm;
+
+	location /hbnb_static {
+		alias /data/web_static/current;
+		index index.html index.htm;
+	}
+
+	location /redirect_me {
+		return 301 https://cuberule.com/;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
